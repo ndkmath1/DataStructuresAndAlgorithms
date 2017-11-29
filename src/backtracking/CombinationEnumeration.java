49aebfd6feee8d8fe325d06e09dc8e0155fe20f0
ndkmath1 <ndkmath1@gmail.com>
2017-11-29 13:47:28 +0700

    combination enumeration

--- src/backtracking/CombinationEnumeration.java
+++ src/backtracking/CombinationEnumeration.java
@@ -8,27 +8,38 @@ import java.util.Scanner;
  *
  */
 public class CombinationEnumeration {
-	
+
 	private int n, k, x[];
-	
+
 	private void readData() {
 		Scanner sc = new Scanner(System.in);
 		n = sc.nextInt();
 		k = sc.nextInt();
-		x = new int[k];
+		x = new int[k + 1];
 	}
-	
+
 	private void attemp(int i) {
-		
+		for (int v = x[i - 1] + 1; v <= n - k + i; ++v) {
+			x[i] = v;
+			if (i == k) {
+				print();
+			} else {
+				attemp(i + 1);
+			}
+		}
 	}
-	
+
 	private void print() {
-		
+		for (int i = 1; i <= k; ++i) {
+			System.out.print(x[i]);
+		}
+		System.out.println();
 	}
-	
+
 	public static void main(String[] args) {
 		CombinationEnumeration ce = new CombinationEnumeration();
 		ce.readData();
+		//x[0] = 0; not need, by default all element in array is 0
 		ce.attemp(1);
 	}
 
