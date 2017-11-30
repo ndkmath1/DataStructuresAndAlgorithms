ab02cf2723f300d5381b7f39694638336519bb04
ndkmath1 <ndkmath1@gmail.com>
2017-11-30 19:37:24 +0700

    Arrangement

--- src/backtracking/Arrangement.java
+++ src/backtracking/Arrangement.java
@@ -7,42 +7,37 @@ import java.util.Scanner;
  * @author khanh 
  * Enumeration A^{k}_{n}
  */
-public class Arrangement {
+class Arrangement {
 
-	private int n, k, x[], mark[];
-	private Scanner sc = new Scanner(System.in);
+	int n, k, x[]; 
+	boolean mark[]; // default, element in mark array is false
+	Scanner sc = new Scanner(System.in);
+	int count;
 
-	private void readData() {
+	void readData() {
 		n = sc.nextInt();
 		k = sc.nextInt();
 		x = new int[k];
-		mark = new int[n];
+		mark = new boolean[n];
 	}
 
-	private void attempt(int i) {
+	void attempt(int i) {
 		for (int v = 0; v < n; ++v) {
-			if (mark[v] == 0) {
+			if (!mark[v]) {
 				x[i] = v;
-				mark[v] = 1; // mark already assign
-			} else {
-				continue;
+				if (i == k - 1) {
+					print();
+				} else {
+					mark[v] = true;
+					attempt(i + 1);
+					mark[v] = false;
+				}
 			}
-			if (i == k - 1) {
-				print();
-			} else {
-				attempt(i + 1);
-				
-			}
-		}
-	}
-	
-	private void reset() {
-		for (int i = 0; i < n; ++i) {
-			mark[i] = 0;
 		}
 	}
 
-	private void print() {
+	void print() {
+		System.out.print(++count + ". ");
 		for (int i = 0; i < k; ++i) {
 			System.out.print(x[i] + 1);
 		}
